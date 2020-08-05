88ae2d141943ff94d7056b466dc94299f288c5aa
harshaltrivedi10 <43525792+harshaltrivedi10@users.noreply.github.com>
2020-08-05 15:00:47 -0700

    simple-counter: Added setState and covered, localStorage and callbacks in incremement

---
+++ simple-counter-react-state/src/Counter.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+const getStateFromLocalStorage = () => {
+  const storage = localStorage.getItem('counterState');
+  if (storage) return JSON.parse(storage);
+  return { count: 0 };
+};
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = getStateFromLocalStorage();
+  }
+
+  increment = () => {
+    this.setState(
+      (state, props) => {
+        const { max, step } = props;
+        if (state.count >= max) return;
+        return { count: state.count + step };
+      },
+      () => {
+        localStorage.setItem('counterState', JSON.stringify(this.state));
+        console.log(localStorage);
+      },
+    );
+
+    console.log('Before!', this.state);
+  };
+
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  reset = () => {
+    this.setState({ count: 0 });
+  };
+
+  render() {
+    const { count } = this.state;
+    return (
+      <div className="Counter">
+        <p className="count">{count}</p>
+        <section className="controls">
+          <button onClick={this.increment}>Increment</button>
+          <button onClick={this.decrement}>Decrement</button>
+          <button onClick={this.reset}>Reset</button>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Counter;
